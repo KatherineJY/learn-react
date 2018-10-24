b560cc381f8de33022809148c3c9b351b0490210
KatherineJY <kellymayliu@163.com>
2018-10-24 21:31:26 +0800

    function args

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,12 +24,20 @@ class Home extends React.Component {
         alert(this.state.msg);
     }
 
+    setMsg = (newMsg)=>{
+        this.setState({
+            msg:newMsg
+        })
+    }
+
     render() {
         return(
             <div>
+                <h2>{this.state.msg}</h2>
                 <button onClick={this.run}>Run function</button>
                 <button onClick={this.getData.bind(this)}>Get data</button>
                 <button onClick={this.getMsg}>Get msg</button>
+                <button onClick={this.setMsg.bind(this,'nnn')}>Set msg</button>
             </div>
         )
     }
