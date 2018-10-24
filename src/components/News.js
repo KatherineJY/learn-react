c4cf7ab3f543595e91149199f28516d27f1ddd28
KatherineJY <kellymayliu@163.com>
2018-10-24 20:59:25 +0800

    array binding

--- src/components/News.js
+++ src/components/News.js
@@ -8,16 +8,40 @@ class News extends React.Component{
 
         this.state = {
             msg:'news',
+            list:['1111','222','333'],
+            list2:[<h2>I am h2</h2>,<h2>I am h2</h2>,<h2>I am h2</h2>],
+            list3:[
+                {title:"news1"},
+                {title:"news2"},
+                {title:"news3"},
+            ]
         }
     }
 
     render(){
+
+        let lResult = this.state.list.map((value,key)=>{
+            return <li key={key}>{value}</li>;
+        })
+
         return(
             <div>
                 {this.state.msg}
                 <img src={pic} alt=""></img>
                 <img src={ require("../assests/images/1.jpg")} alt=""></img>
-                
+                <hr/>
+
+                {/* {this.state.list2} */}
+
+                {
+                    lResult
+                }
+
+                {
+                    this.state.list3.map((value,key)=>{
+                        return (<li key={key}>{value.title}</li>);
+                    })
+                }
             </div>
         )
     }
