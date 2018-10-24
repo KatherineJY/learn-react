4647e0ef7960fe42e8bd37414509f435bb1f7f28
KatherineJY <kellymayliu@163.com>
2018-10-24 20:21:50 +0800

    Add componment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Home from './components/Home'
+// import Home from './components/Home'
+import News from './components/News'
 
 class App extends Component {
   render() {
     return (
-      <Home></Home>
+      <div>
+        <News/>
+      </div>
     );
   }
 }
