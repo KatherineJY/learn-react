976b48292068797b7c62657201d9f7770a9a80fe
KatherineJY <kellymayliu@163.com>
2018-10-25 16:12:49 +0800

    Todolist

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import './App.css';
 // import Home from './components/Home'
 // import List from './components/List'
 import ToDoList from './components/ToDoList'
-import ReactForm from './components/ReactForm'
+//import ReactForm from './components/ReactForm'
 
 class App extends Component {
   render() {
     return (
       <div>
         <ToDoList />
-        <ReactForm />
       </div>    
     );
   }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -5,21 +5,49 @@ class ToDoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username:""
+            list:[
+
+            ]
         };
     }
-    
-    inputChange = (e) => {
+
+    addData = () => {
+        //alert(this.refs.title.value);
+        let tempList = this.state.list;
+        tempList.push(this.refs.title.value);
+        this.refs.title.value="";
         this.setState({
-            username:e.target.value
-        });
+            list:tempList
+        })
+    }
+    
+    deleteData = (key) => {
+        let tempList = this.state.list;
+        tempList.splice(key,1);
+        this.setState({
+            list:tempList
+        })
     }
 
     render() {
         return (
             <div>
-                <input defaultValue={this.state.username} onChange={this.inputChange}/>
-                {this.state.username}
+                <h2>Todolist</h2>
+
+                <input ref="title"/><button onClick={this.addData}>Add</button>
+
+                <ul>
+                    {
+                        this.state.list.map((value,key) => {
+                            return (
+                                <li key={key}>
+                                    {value}
+                                    <button onClick={this.deleteData.bind(this,key)}>Delete</button>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         );
     }
